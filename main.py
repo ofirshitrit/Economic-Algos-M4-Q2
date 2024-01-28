a12211880bf2a19024dbc533ad480d4867639eba
OfirShitrit <ofirha18@gmail.com>
2024-01-28 17:36:46 +0200

    add rule2

--- main.py
+++ main.py
@@ -49,33 +49,30 @@ def create_results(path):
         elif state.valuation_of_player1 != next_state.valuation_of_player1:
             # player 1 take the current object
             results[1].append(state.number_of_objects)
-    else:
-        print("Target state not found in the tree.")
-
     print("\nResults: ", results)
     return results
 
 
-def print_results(results, valuations):
-    total_value = 0
+def print_player_results(player, results, final_state):
+    objects = ', '.join(map(str, results[player]))
+    total_value = getattr(final_state, f'valuation_of_player{player}')
+    print(f"player {player} gets items {objects} with values {total_value}")
 
-    for player in results:
-        objects = ', '.join(map(str, results[player]))  # print the list without the [ ]
-        for object in results[player]:
-            value = valuations[player][object]
-            total_value += value
-        print(f"player {player} gets items {objects} with values {total_value}")
-        total_value = 0
+def print_results(results, valuations, final_state):
+    print_player_results(0, results, final_state)
+    print_player_results(1, results, final_state)
 
 
-def add_new_states(current_states):
+def add_new_states(current_states, valuations):
     new_states = []
     for state in current_states:
         # For every new state - nether player 0 gets the object or player 1 gets the object
         new_state_for_player0 = State(state.number_of_objects + 1,
                                       state.valuation_of_player0 + valuations[0][state.number_of_objects],
                                       state.valuation_of_player1)
-        new_state_for_player1 = State(state.number_of_objects + 1, state.valuation_of_player0,
+
+        new_state_for_player1 = State(state.number_of_objects + 1,
+                                      state.valuation_of_player0,
                                       state.valuation_of_player1 + valuations[1][state.number_of_objects])
 
         state.left = new_state_for_player0
@@ -93,7 +90,7 @@ def get_states(valuations: list[list[float]], initial_state):
 
     for i in range(number_of_objects):
         print(f"\nObject {i}: ")
-        new_states = add_new_states(current_states)
+        new_states = add_new_states(current_states, valuations)
 
         print("The new states: ")
         for i, state in enumerate(new_states):
@@ -111,7 +108,19 @@ def egalitarian_allocation(valuations: list[list[float]]):
     path = find_path(initial_state, final_state)
     print_path(path)
     results = create_results(path)
-    print_results(results, valuations)
+    print_results(results, valuations, final_state)
+    return final_state
+
+
+def max_product_allocation(valuations: list[list[float]]):
+    initial_state = State(0, 0, 0)  # Assuming initial product is 1 for both players
+    states = get_states(valuations, initial_state)
+    final_state = max(states,
+                      key=lambda state: state.valuation_of_player0 * state.valuation_of_player1)
+    path = find_path(initial_state, final_state)
+    print_path(path)
+    results = create_results(path)
+    print_results(results, valuations, final_state)
     return final_state
 
 
@@ -119,10 +128,13 @@ if __name__ == '__main__':
     # valuations = [[4, 5, 6, 7, 8], [8, 7, 6, 5, 4]]
     # valuations = [[4, 5, 6], [6, 5, 4]]
     # valuations = [[11, 55], [33, 44]]
-    # valuations = [[11, 55, 66], [33, 44, 22]]
+    valuations = [[11, 55, 66], [33, 44, 22]]
     # valuations = [[11, 66], [44, 22]]
-    valuations = [[11, 22, 33, 44], [22, 11, 44, 33]]
+    # valuations = [[11, 22, 33, 44], [22, 11, 44, 33]]
     # results = {0: [1], 1: [0]}
     # print_outputs(results, valuations)
     result_state = egalitarian_allocation(valuations)
     print(f"\nEgalitarian Allocation: {result_state}")
+
+    # result_state1 = max_product_allocation(valuations)
+    # print(f"\nmax_product_allocation: {result_state1}")
--- pruning_rules.py
+++ pruning_rules.py
@@ -1,5 +1,6 @@
 import random
 
+from State import State
 from main import get_states, add_new_states
 
 
@@ -7,13 +8,23 @@ from main import get_states, add_new_states
 
 # Rule 1: delete identical states
 def pruning_rule1(states):
-    for curr_state in states:
-        for state in states:
-            if curr_state.isEqual(state):
-                states.remove(state)
+    new_states = []
+    length = len(states)
+
+    for i in range(length):
+        curr_state = states[i]
+
+        # Check if curr_state is already in new_states
+        if curr_state not in new_states:
+            new_states.append(curr_state)
+
+    states.clear()
+    states.extend(new_states)
 
 
-def egalitarian_allocation_rule1(valuations: list[list[float]], initial_state):
+
+def egalitarian_allocation_rule1(valuations: list[list[float]]):
+    initial_state = State(0, 0, 0)
     states = get_states(valuations, initial_state)
     pruning_rule1(states)
     max_min_value_state = max(states,
@@ -35,10 +46,9 @@ state.valuation_of_player1)
 """
 
 
-def get_pessimistic_bound(state, total_objects, valuations):  # TODO
-    remaining_objects = total_objects - state.number_of_objects
-    random_choice = random.randint(0, 1)
-    for object in remaining_objects:
+def get_pessimistic_bound(state, remaining_objects, valuations):
+    for object in range(remaining_objects):
+        random_choice = random.randint(0, 1)
         if random_choice == 0:
             state.valuation_of_player0 += valuations[0][object]
         else:
@@ -48,14 +58,51 @@ def get_pessimistic_bound(state, total_objects, valuations):  # TODO
     return pessimistic_bound
 
 
-
-def get_optimistic_bound(state, remaining_objects):  # TODO
-    pass
+def get_optimistic_bound(state, remaining_objects, valuations):
+    for object in range(remaining_objects):
+        state.valuation_of_player0 += valuations[0][object]
+        state.valuation_of_player1 += valuations[1][object]
+        state.number_of_objects = state.number_of_objects + 1
+    optimistic_bound = min(state.valuation_of_player0, state.valuation_of_player1)
+    return optimistic_bound
 
 
 def pruning_rule2(states, total_objects, valuations):
+    max_pessimistic_bound = 0
     for state in states:
-        pessimistic_bound = get_pessimistic_bound(state, total_objects, valuations)
-        optimistic_bound = get_optimistic_bound(state, total_objects)
-        if optimistic_bound <= pessimistic_bound:
+        remaining_objects = total_objects - state.number_of_objects
+
+        pessimistic_bound = get_pessimistic_bound(state, remaining_objects, valuations)
+        if pessimistic_bound > max_pessimistic_bound:
+            max_pessimistic_bound = pessimistic_bound
+
+        optimistic_bound = get_optimistic_bound(state, remaining_objects, valuations)
+
+        if optimistic_bound < pessimistic_bound:
             states.remove(state)
+
+
+def egalitarian_allocation_rule2(valuations: list[list[float]]):
+    initial_state = State(0, 0, 0)
+    states = get_states(valuations, initial_state)
+    pruning_rule2(states, len(valuations[0]), valuations)
+    max_min_value_state = max(states,
+                              key=lambda state: min(state.valuation_of_player0, state.valuation_of_player1))
+    return max_min_value_state
+
+
+if __name__ == '__main__':
+    # valuations = [[4, 5, 6, 7, 8], [8, 7, 6, 5, 4]]
+    # valuations = [[4, 5, 6], [6, 5, 4]]
+    # valuations = [[11, 55], [33, 44]]
+    valuations = [[11, 55, 66], [33, 44, 22]]
+    # valuations = [[11, 66], [44, 22]]
+    # valuations = [[11, 22, 33, 44], [22, 11, 44, 33]]
+    # results = {0: [1], 1: [0]}
+    # print_outputs(results, valuations)
+
+    # rule1_res = egalitarian_allocation_rule1(valuations)
+    # print(f"\nrule1_res: {rule1_res}")
+
+    rule2_res = egalitarian_allocation_rule2(valuations)
+    print(f"\nrule2_res Allocation: {rule2_res}")
